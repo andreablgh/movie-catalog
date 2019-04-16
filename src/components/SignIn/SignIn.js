9e6d6c103c6c44fad3f57f73e896d16f2943392a
Andrea <balogh_andy95@yahoo.com>
2019-04-16 13:39:29 +0300

    Sign up form design changes

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
 import './sign.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 
 class SignIn extends Component {
   render() {
     return (
-    <div className="wrapper">
+      <div className="cont">
+            <Link to="/" id="back-btn"><FontAwesomeIcon icon={faAngleLeft} /></Link>
+      <div className="wrapper">
       <div className="box-one">
         <h1 className="sign-up-title">Sign up</h1>
         <form className="signin">
@@ -12,9 +20,17 @@ class SignIn extends Component {
             <input className="login-input" type="text" placeholder="Enter email..."/>
             <input className="login-input" type="password" placeholder="Password..."/>
           <button className="submit" type="submit">Sign up</button>
+          <hr/>
+          <small> Or sign up with </small>
+          <div id="or-sign">
+            <Link to="/" className="sign-btn"><FontAwesomeIcon icon={faFacebook} /></Link>
+            <Link to="/" className="sign-btn"><FontAwesomeIcon icon={faInstagram} /></Link>
+            <Link to="/" className="sign-btn"><FontAwesomeIcon icon={faEnvelope} /></Link>
+          </div>
         </form>
       </div>
-      </div>
+    </div>
+    </div>
     )
   }
 }
